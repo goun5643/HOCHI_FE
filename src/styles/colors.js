a998b0dd06d2013e57b9861d9511492887e1de97
goun5643 <seogoun45@gmail.com>
2025-05-20 14:42:39 +0900

    colors.js 작성

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+export default {
+  // GrayScale
+  gray100: '#F3F3F2',
+  gray200: '#E6E5E5',
+  gray300: '#CAC7C5',
+  gray400: '#F4F2F1',
+
+  // White/Black
+  white000: '#FBFBFA',
+  black000: '#1B1A18',
+
+  // Blue
+  Blue001: '#1562B2',
+  Blue002: '#56C2E8',
+};
